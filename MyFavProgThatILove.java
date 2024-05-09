92ddd34dbf03f796bfdf9c15e4723671f95e8534
thotaneysa09 <168233890+thotaneysa09@users.noreply.github.com>
2024-05-08 20:01:11 -0500

    Update MyFavProgThatILove.java
    all floors made but no collisions yet

--- MyFavProgThatILove.java
+++ MyFavProgThatILove.java
@@ -1,18 +1,12 @@
 /**
-* Creates game panel
+*add purpose of program
 *
 * @author Neysa Thota
 * @version 1.0
-* @since 4/22/2024
+* date: 4/22/2024
 */ 
-
-import java.io.*;//needed to generate random values
-import java.util.*;//neede for scanner
 import java.awt.*;//needed for graphic 
-import javax.swing.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.*;
-
+import javax.swing.*;//needed for the JFrame
 
 public class MyFavProgThatILove
 {	
@@ -22,13 +16,39 @@ public class MyFavProgThatILove
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setVisible(true);
-		frame.setTitle("A Creative Title");
+		frame.setTitle("Haunted Haven");
 		
 		GamePanel panel = new GamePanel();
 		
 		frame.add(panel);
 		frame.pack();
-		
+
+
 		panel.startGameThread();//starting game thread
+		/*String[] yesNo = {"Yes","No"};
+		String input = (String) JOptionPane.showInputDialog(null,"Do you want to playe? (Y/N): ", "", JOptionPane.QUESTION_MESSAGE, null, yesNo, yesNo[1]);
+		
+		if (input.equals("Yes"))
+		{
+			panel.startGameThread();//starting game thread
+		}
+		else 
+		{
+			frame.setVisible(false); //you can't see me!
+			frame.dispose(); //Destroy the JFrame object
+		}*/
+		
+		/*int dialogButton = JOptionPane.YES_NO_OPTION;
+        dialogButton = JOptionPane.showConfirmDialog (null, "DO You want to proceed?","WAING", dialogButton);
+        if(dialogButton == JOptionPane.NO_OPTION) 
+        {
+			frame.setVisible(false);
+			frame.dispose(); //Destroys the JFrame object
+			System.exit(0);
+        }
+        else if (dialogButton == JOptionPane.YES_OPTION) 
+        {
+			panel.startGameThread();//starting game thread
+		}*/
 	}
 }
