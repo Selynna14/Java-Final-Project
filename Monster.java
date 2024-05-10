69e7e44814934529a686e352a26e0b4818c43dbe
Selynna14 <150946616+Selynna14@users.noreply.github.com>
2024-05-10 08:33:35 -0500

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -3,43 +3,55 @@ import java.util.* //for using random library.
 Public class Monsters 
 {//start of monsters class
   //make instance variables 
-  int Damage;
-  int Health;
-  int Speed;
+  private int monsterDamage;
+  private int monsterHealth;
+  private int monsterSpeed;
 
   Public BloodThirstyPixies()
   {//3 Pixies
-		Health = 10;
-		Speed=8;
-    Damage = randObject.nextInt(10,13);	//random integer from 10-12
+	monsterHealth = 10;
+	monsterSpeed=8;
+    	monsterDamage = randObject.nextInt(10,13);	//random integer from 10-12
   }
 
   Public Witches()
   {
   //only 2 Witches
-  	Health = 30;
-    Speed = 4;
-    Damage = randObject.nextInt(5,8);//random integer from 5-7 	
+  	monsterHealth = 30;
+    monsterSpeed = 4;
+    monsterDamage = randObject.nextInt(5,8);//random integer from 5-7 	
   }
   
   Public Zombies()
   {
-  	Health = 10;
-  	Speed=2;
-    Damage = randObect.nextInt(0,5); //random integer from 0-4 
+  	monsterHealth = 10;
+  	monsterSpeed=2;
+    monsterDamage = randObect.nextInt(0,5); //random integer from 0-4 
   }
   
   Public Skeletons()
   {
-  	Health = 13;
-    Speed=3;
-    Damage = randObect.nextInt(4,8);//random integer from 4-7  
+  	monsterHealth = 13;
+    monsterSpeed=3;
+    monsterDamage = randObect.nextInt(4,8);//random integer from 4-7  
   }
   Public House()
   {
-  	Health = 75;
-    Speed = null;
-    Damage = randObject.nextInt(10,21); //random integer from 10-20 
+  	monsterHealth = 75;
+    monsterSpeed = null;
+    monsterDamage = randObject.nextInt(10,21); //random integer from 10-20 
   }
+public int getMonsterHealth()
+{
+	return monsterHealth;
+}
+public int getMonsterDamage()
+{
+	return monsterDamage;
+}
+public int getMonsterSpeed()
+{
+	return monsterSpeed;
+}
 
 }//end of monsters class
