4045ee6964e98e446f22c3495c5b08521500a76a
TateSommer <168102846+TateSommer@users.noreply.github.com>
2024-05-13 08:20:42 -0500

    Update MyFavProgThatILove.java

--- MyFavProgThatILove.java
+++ MyFavProgThatILove.java
@@ -25,7 +25,7 @@ public class MyFavProgThatILove
 
 
 		panel.startGameThread();//starting game thread
-		/*String[] yesNo = {"Yes","No"};
+		String[] yesNo = {"Yes","No"};
 		String input = (String) JOptionPane.showInputDialog(null,"Do you want to play? ", "", JOptionPane.QUESTION_MESSAGE, null, yesNo, yesNo[1]);
 		
 		if (input.equals("Yes"))
@@ -33,12 +33,26 @@ public class MyFavProgThatILove
 			panel.startGameThread();//starting game thread
    			String[] loadFile = {"1","2", "3"};
 			String input2 = (String) JOptionPane.showInputDialog(null,"Which file do you want to play out of? ", "", JOptionPane.QUESTION_MESSAGE, null,loadFile, loadFile[2]);
+			if (input2.equals("1"))
+			{
+
+			}
+			else if (input2.equals("2"))
+			{
+
+			}
+			else if (input2.equals("3"))
+			{
+
+			}
+			else
+				System.out.println("?");
 		}
 		else 
 		{
 			frame.setVisible(false); //you can't see me!
 			frame.dispose(); //Destroy the JFrame object
-		}*/
+		}
 		
 		/*int dialogButton = JOptionPane.YES_NO_OPTION;
         dialogButton = JOptionPane.showConfirmDialog (null, "DO You want to proceed?","WAING", dialogButton);
