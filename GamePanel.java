c669c054f435ae6637132c266a525835b55bc3ba
Selynna14 <150946616+Selynna14@users.noreply.github.com>
2024-05-14 09:14:44 -0500

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -58,7 +58,7 @@ public class GamePanel extends JPanel implements Runnable, KeyListener
 		this.addKeyListener(this);
 		this.setFocusable(true);
 		
-		tile = new Tile[11];
+		tile = new Tile[37];
 		mapTileNum = new int [maxWorldCol][maxWorldRow];
 		
 	}
@@ -71,40 +71,67 @@ public class GamePanel extends JPanel implements Runnable, KeyListener
 			tile[0].image = ImageIO.read(new File("Floorboard.png"));
 			
 			tile[1] = new Tile();
-			tile[1].image = ImageIO.read(new File("Wall.png"));
+			tile[1].image = ImageIO.read(new File("Grayfloor.png"));
 			tile[1].collison = true;
 			
 			tile[2] = new Tile();
-			tile[2].image = ImageIO.read(new File("Window.png"));
+			tile[2].image = ImageIO.read(new File("Wall.png"));
 			
 			tile[3] = new Tile();
 			tile[3].image = ImageIO.read(new File("stairs.png"));
 			
 			tile[4] = new Tile();
-			tile[4].image = ImageIO.read(new File("chest.png"));
+			tile[4].image = ImageIO.read(new File("Improved_stairs.png"));
 			
 			tile[5] = new Tile();
-			tile[5].image = ImageIO.read(new File("cabinet.png"));
+			tile[5].image = ImageIO.read(new File("Window.png"));
 			
 			tile[6] = new Tile();
 			tile[6].image = ImageIO.read(new File("door.png"));
-			
-			tile[7] = new Tile();
-			tile[7].image = ImageIO.read(new File("table.png"));
-			
-			tile[8] = new Tile();
-			tile[8].image = ImageIO.read(new File("Chair.png"));
-			
-			tile[9] = new Tile();
-			tile[9].image = ImageIO.read(new File("Pile_of_clothes.png"));
-			
+			//furniture
 			tile[10] = new Tile();
-			tile[10].image = ImageIO.read(new File("Improved_stairs.png"));
+			tile[10].image = ImageIO.read(new File("Chair.png"));
+
+			tile[11] = new Tile();
+			tile[11].image = ImageIO.read(new File("Table.png"));
+
+			tile[12] = new Tile();
+			tile[12].image = ImageIO.read(new File("Cabinet.png"));
+
+			tile[13] = new Tile();
+			tile[13].image = ImageIO.read(new File("chest.png"));
+
+			tile[14] = new Tile();
+			tile[14].image = ImageIO.read(new File("Pile_of_clothes.png"));
+
+			tile[15] = new Tile();
+			tile[15].image = ImageIO.read(new File("Scroll.png"));
+			// monsters
+			tile[20] = new Tile();
+			tile[20].image = ImageIO.read(new File("Zombie.png"));
+
+			tile[21] = new Tile();
+			tile[21].image = ImageIO.read(new File("Skeleton.png"));
+
+			tile[22] = new Tile();
+			tile[22].image = ImageIO.read(new File("Witch.png"));
+
+			tile[23] = new Tile();
+			tile[23].image = ImageIO.read(new File("Pixies.png"));
+
+			tile[24] = new Tile();
+			tile[24].image = ImageIO.read(new File("Soul_orb.png"));
+			//weapons
+			tile[30] = new Tile();
+			tile[30].image = ImageIO.read(new File("Woodenaxe.png"));
+			
+			tile[31] = new Tile();
+			tile[31].image = ImageIO.read(new File("Ironsword.png"));
 			
 		}
 		catch(Exception e)
 		{
-			System.out.println(e + "jhskadhsk");
+			System.out.println(e + " ");
 		}
 	}
 	
