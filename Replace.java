c8a5c7a606f65e8b3476f6757b98c604047cf6a6
TateSommer <168102846+TateSommer@users.noreply.github.com>
2024-05-21 07:35:31 -0500

    Update Replace.java

--- Replace.java
+++ Replace.java
@@ -51,8 +51,62 @@ public class replace
 		} // end catch
 	} // ends replaceWeapons
 
-	public replaceMonster(int row, int collum)
+	public replaceMonster(int monsternum, int currLevel)
 	{ // starts replaceMonster
-		panel.mapTileNum[collum][row] = 0; // sets defeated monster tile to 0
+		if (currLevel == 2)
+		{
+			try
+			{ // Starts try
+				ArrayList<String[]> wholeText = new ArrayList<String[]>(); // create arrayList
+				File newFile = new File("Map01Floor1.txt"); // finds the file
+				Scanner iR = new Scanner(newFile); // creates scanner to read file
+				while(iR.hasNextLine()) // creates a loop while there is more data
+				{ // Starts while loop
+					String theLine = iR.nextLine();
+					wholeText.add(theLine.split(" "));
+				} // Ends while loop
+				iR.close(); // closes the input reader
+				for(int i = 0; i < wholeText.size(); i++)
+				{ // Starts for
+					if (i > blank)
+					{
+						String[] currLine = wholeText.get(i);
+						for(int j = 0; j < currLine.length; j++)
+						{ // Starts for
+							
+							String word = currLine[j];
+							if(word.equals("monsternum")) // Looks weapon number and replaces it with 0
+							{ // Start if
+								System.out.print("0 ");
+							} // Ends if
+							else
+							{
+								System.out.print(word + " "); // Prints every other word
+							}
+						} // ends for	
+					} // ends if
+					else
+					{
+						String[] currLine = wholeText.get(i);
+						for(int j = 0; j < currLine.length; j++)
+						{ // Starts for
+							String word = currLine[j];
+							System.out.print(word + " "); // Prints every other word
+						}
+					} // ends else
+					
+				System.out.println();
+				} // end file loop
+	
+				
+			} // end try
+			catch(Exception e) // stops the program from breaking
+			{ // start catch
+				System.out.println("Error!");
+			} // end catch
+		}
+		if (currLevel == 3)
+		{
+		}
 	} // ends replaceMonster
 } // ends
