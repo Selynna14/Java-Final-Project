31e69ff323b9d22d9969bf359e5aec6c69ee35cc
TateSommer <168102846+TateSommer@users.noreply.github.com>
2024-05-28 08:31:25 -0500

    Update Sommer_saveFiles.java

--- Sommer_saveFiles.java
+++ Sommer_saveFiles.java
@@ -4,33 +4,91 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * This class reads data from a file and saves it into variables
+ * This method reads data from a file and saves it into variables
  * @author Tate Sommer
  * @version 1.0
  * date: 5/08/2024
 */
 
-public class Sommer_saveFiles
-{ // start class 		
-	try
-	{ // starts try
-		File fileOne = new File("fileOne.txt"); // Go find and load file
-		if (fileOne.createNewFile())
-		{
-			System.out.println("Good");
-		}
-		else
-		{
-			String name = Files.readAllLines(Paths.get("fileOne.txt")).get(0);
-			String line2 = Files.readAllLines(Paths.get("fileOne.txt")).get(1);
-			String line3 = Files.readAllLines(Paths.get("fileOne.txt")).get(2);
-			int health = Integer.parseInt(line2);
-			int floor = Integer.parseInt(line3);
-		}
+public Sommer_saveFiles()
+{ // start method
+	String[] loadFile = {"1","2", "3"};
+	String input2 = (String) JOptionPane.showInputDialog(null,"Which file do you want to play out of? ", "", JOptionPane.QUESTION_MESSAGE, null,loadFile, loadFile[2]);
+	if (input2.equals("1"))
+	{
+		try
+		{ // starts try
+			File fileOne = new File("fileOne.txt"); // Go find and load file
+			if (fileOne.createNewFile())
+			{
+				System.out.println("Good");
+			}
+			/*else
+			{
+				name = Files.readAllLines(Paths.get("fileOne.txt")).get(0);
+				line2 = Files.readAllLines(Paths.get("fileOne.txt")).get(1);
+				floor = Files.readAllLines(Paths.get("fileOne.txt")).get(2);
+				health = Integer.parseInt(line2);
+			}
+			*/
+		} // ends try
+		catch(Exception e)
+		{ // starts catch
+			System.out.println("Error! " + e);
+		} // ends catch
+	
+	}
+	else if (input2.equals("2"))
+	{
+		try
+		{ // starts try
+			File fileTwo = new File("fileTwo.txt"); // Go find and load file
+			if (fileTwo.createNewFile())
+			{
+				System.out.println("Good");
+			}
+			/*else
+			{
+				name = Files.readAllLines(Paths.get("fileTwo.txt")).get(0);
+				line2 = Files.readAllLines(Paths.get("fileTwo.txt")).get(1);
+				floor = Files.readAllLines(Paths.get("fileTwo.txt")).get(2);
+				health = Integer.parseInt(line2);
+			}
+			*/
+		} // ends try
+		catch(Exception e)
+		{ // starts catch
+			System.out.println("Error! " + e);
+		} // ends catch
 
-	} // ends try
-	catch(Exception e)
-	{ // starts catch
-		System.out.println("Error! " + e);
-	} // ends catch
-} // ends class
+	}
+	else if (input2.equals("3"))
+	{
+		try
+		{ // starts try
+			File fileThree = new File("fileThree.txt"); // Go find and load file
+			if (fileThree.createNewFile())
+			{
+				System.out.println("Good");
+			}
+			//else
+			//{
+						/* 
+				 * name = Files.readAllLines(Paths.get("fileThree.txt")).get(0);
+				line2 = Files.readAllLines(Paths.get("fileThree.txt")).get(1);
+				floor = Files.readAllLines(Paths.get("fileThree.txt")).get(2);
+				health = Integer.parseInt(line2);
+						*/
+			//}
+			
+		} // ends try
+		catch(Exception e)
+		{ // starts catch
+			System.out.println("Error! " + e);
+		} // ends catch
+
+	}
+	else
+		System.out.println("?");
+		
+} // ends method
