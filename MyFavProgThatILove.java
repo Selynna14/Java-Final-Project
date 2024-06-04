6f4eadf09da8e202a4d7f101c286be031e33bdc4
TateSommer <168102846+TateSommer@users.noreply.github.com>
2024-06-03 22:11:36 -0500

    Update MyFavProgThatILove.java

--- MyFavProgThatILove.java
+++ MyFavProgThatILove.java
@@ -68,6 +68,7 @@ public class MyFavProgThatILove
 		
 		WeponList.add("Hand");
 		// code
+		/**
 		String[] loadFile = {"1","2", "3"};
 		String input5 = (String) JOptionPane.showInputDialog(null,"Which file do you want to play out of? ", "", JOptionPane.QUESTION_MESSAGE, null,loadFile, loadFile[2]);
 		if (input5.equals("1"))
@@ -79,6 +80,74 @@ public class MyFavProgThatILove
 				{
 					System.out.println("Good");
 				}
+				else
+				{
+					name = Files.readAllLines(Paths.get("fileOne.txt")).get(0);
+					line2 = Files.readAllLines(Paths.get("fileOne.txt")).get(1);
+					floor = Files.readAllLines(Paths.get("fileOne.txt")).get(2);
+					health = Integer.parseInt(line2);
+				}
+				
+			} // ends try
+			catch(Exception e)
+			{ // starts catch
+				System.out.println("Error! " + e);
+			} // ends catch
+		
+		}
+		else if (input5.equals("2"))
+		{
+			try
+			{ // starts try
+				File fileTwo = new File("fileTwo.txt"); // Go find and load file
+				if (fileTwo.createNewFile())
+				{
+					System.out.println("Good");
+				}
+				else
+				{
+					name = Files.readAllLines(Paths.get("fileTwo.txt")).get(0);
+					line2 = Files.readAllLines(Paths.get("fileTwo.txt")).get(1);
+					floor = Files.readAllLines(Paths.get("fileTwo.txt")).get(2);
+					health = Integer.parseInt(line2);
+				}
+				
+			} // ends try
+			catch(Exception e)
+			{ // starts catch
+				System.out.println("Error! " + e);
+			} // ends catch
+
+		}
+		else if (input5.equals("3"))
+		{
+			try
+			{ // starts try
+				File fileThree = new File("fileThree.txt"); // Go find and load file
+				if (fileThree.createNewFile())
+				{
+					System.out.println("Good");
+				}
+			        else
+				{
+							
+					name = Files.readAllLines(Paths.get("fileThree.txt")).get(0);
+					line2 = Files.readAllLines(Paths.get("fileThree.txt")).get(1);
+					floor = Files.readAllLines(Paths.get("fileThree.txt")).get(2);
+					health = Integer.parseInt(line2);
+							
+				}
+				
+			} // ends try
+			catch(Exception e)
+			{ // starts catch
+				System.out.println("Error! " + e);
+			} // ends catch
+
+		}
+		else
+			System.out.println("?");
+		**/
 			}
 			catch(Exception e)
 			{
